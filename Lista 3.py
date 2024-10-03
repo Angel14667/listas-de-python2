896b2db9537e30759cef8f40d8f6c794fa22109b
Angel Manuel Guevara Cano <230i0021@tecmartinez.edu.mx>
2024-10-03 10:24:01 -0600

    Add files via upload

---
+++ Lista 3.py
@@ -0,0 +1,8 @@
+# Define una lista con tres elementos
+l = [4, 9, 16]
+
+# Desempaqueta la lista en tres variables
+x, y, z = l
+
+# Imprime los valores de las variables x, y, z
+print(x, y, z)  # imprime 4 9 16
